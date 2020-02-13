ec7bffcfa6ca534fe32b50a9c173df3844ad8314
Guoxi Wang <guoxizacks@gmail.com>
2020-02-13 10:15:48 -0800

    update start_postgres to set max_connections

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run --name cs223_postgres --rm --volume=$(pwd)/postgres_data:/var/lib/postgresql/data -p 10020:5432 -e POSTGRES_PASSWORD=password -d postgres:12.1
+docker container stop cs223_postgres
+docker run --name cs223_postgres --rm --volume=$(pwd)/postgres_data:/var/lib/postgresql/data -p 10020:5432 -e POSTGRES_PASSWORD=password -d postgres:12.1 -N 1000
 
